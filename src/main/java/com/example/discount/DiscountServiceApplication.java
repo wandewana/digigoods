fad3571f6276c1f40785800de15291b96bc05d63
Daya Adianto <addianto@users.noreply.github.com>
2025-07-29 07:50:10 +0700

    Initial commit

---
+++ src/main/java/com/example/discount/DiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountServiceApplication.class, args);
+	}
+
+}
